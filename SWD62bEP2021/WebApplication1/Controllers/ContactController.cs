29ce8b6450218882857a0210bc6eba252eee5161
Kurt <kurtagius613@gmail.com>
2021-10-12 18:30:20 +0200

    Initial Commit

---
+++ SWD62bEP2021/WebApplication1/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // Role: To render the page
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Role: To service the button submit
+        [HttpPost]
+
+        public IActionResult HandleQuery(string query)
+        {
+            return View();
+        }
+    }
+}
