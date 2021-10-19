3a356437fe57473082525baca41c2bada4c86eda
Kurt <kurtagius613@gmail.com>
2021-10-19 15:53:02 +0200

    Good for assignment

--- SWD62bEP2021/WebApplication1/Controllers/ContactController.cs
+++ SWD62bEP2021/WebApplication1/Controllers/ContactController.cs
@@ -20,7 +20,21 @@ namespace WebApplication1.Controllers
 
         public IActionResult HandleQuery(string query)
         {
-            return View();
+
+            // 1st approach: ViewBag is a way how you can pass any type of data from the Controller to the View.
+            // 2nd approach: using models and then you pass the model instance to the View.
+            // 3rd approach: ViewData is like ViewBag but you have to se it as a dictionary.
+            // 4th approach: TempData is like ViewData but it survives postbacks.
+            // 5th approach: Sessions is an upgrade to Tempdata because it survives all the postbacks.
+
+            if (string.IsNullOrEmpty(query) == false)
+                ViewBag.Message = "Thanks. We've received your query.";
+            else
+                ViewBag.Error = "No query received. Please type something.";
+            
+            
+
+            return View("index");
         }
     }
 }
