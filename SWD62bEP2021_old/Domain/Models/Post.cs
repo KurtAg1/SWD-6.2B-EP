0982c1d609e201df2a1620f83cd0366d0aab804d
Kurt <kurtagius613@gmail.com>
2021-10-21 10:23:17 +0200

    Connecting to DB

---
+++ SWD62bEP2021_old/Domain/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string author { get; set; }
+        public DateTime DateCreated { get; set; }
+        [ForeignKey("Blog")]
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
